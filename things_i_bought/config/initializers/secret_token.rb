f78d758220edd061c00e1548cc443bb9eeb36786
Prince Blessing Isang <isangblessing@gmail.com>
2011-10-21 17:42:07 +0100

    I  did my first rails new bla bla bla, bundle install, rails server and Yahoo...

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = 'f982f9fdaab2a38da0014c47ef4d68efd0c4804d1c3310cce0a75f2579f44b78b5bf01f43ba352d4cc71a808e27cbf200c05f2e7f963880e00cfe168a9a90f95'
