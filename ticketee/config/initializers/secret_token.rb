8626b21b4807ab6865bd1b0e1f603a7757035d40
Prince Blessing Sunday <isangblessing@gmail.com>
2011-10-21 22:12:41 +0100

    Generated a new Rails Application called Ticketee for some more serious BDDing.

---
+++ ticketee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'f0c84dda50d37bacb3c41ea4f3c3bc2b6a29d006e315ea5b923b36b85d0d054840d8dcaa4195f4591beab5303d37b2ba1c5e1c56f0b5703b2221f6315dcde8b0'
