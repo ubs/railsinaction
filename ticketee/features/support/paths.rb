019efc12b51dc7b029202bf2b6ae823f568e653e
Prince Blessing Sunday <isangblessing@gmail.com>
2011-10-26 12:10:05 +0100

    Added title functionality for show page using application helper

--- ticketee/features/support/paths.rb
+++ ticketee/features/support/paths.rb
@@ -11,7 +11,7 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
 
-    when /the project page for "([^\"]*)"/
+      when /the project page for "([^\"]*)"/
       project_path(Project.find_by_name!($1))
 
     # Add more mappings here.
