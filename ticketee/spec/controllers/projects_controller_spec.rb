390f1d16bcc843f9ad7ca3176be5ca237938cebb
Prince Blessing Sunday <isangblessing@gmail.com>
2011-11-19 13:46:26 +0000

    Committed with KNOWN fault in specs, they just refuse to run for no reason.

--- ticketee/spec/controllers/projects_controller_spec.rb
+++ ticketee/spec/controllers/projects_controller_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe ProjectsController do
+  let (:user) do
+    user = Factory(:user)
+    user.confirm!
+    return user
+  end
+
+  context "standard users" do
+    it "cannot access the new action" do
+      sign_in(:user, user)
+    end
+  end
+
   it "displays an error for a missing project" do
     get :show, :id => "not-here"
     response.should redirect_to(projects_path)
