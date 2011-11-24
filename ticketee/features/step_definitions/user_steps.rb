aac42adc6996f92f96f68318377a93e3ed71cb42
Prince Blessing Sunday <isangblessing@gmail.com>
2011-11-24 17:25:04 +0000

    Ticketee: Lock down specific projects controller actions for admins only

--- ticketee/features/step_definitions/user_steps.rb
+++ ticketee/features/step_definitions/user_steps.rb
@@ -17,3 +17,9 @@ Given /^I am signed in as them$/ do
     Then I should see "Signed in successfully."
   })
 end
+
+Given /^I am signed in as "([^"]*)"$/ do |email|
+  @user = User.find_by_email!(email)
+  steps("Given I am signed in as them")
+end
+
