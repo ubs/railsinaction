4ebacdf05d5c0d575e7b2e72d141fc6f4bd71872
Blessing Udoisang <blessing@hedtek.com>
2011-12-09 18:03:00 +0000

    Ticketee: Added the ability to create users through the administrative backend

--- ticketee/app/controllers/admin/users_controller.rb
+++ ticketee/app/controllers/admin/users_controller.rb
@@ -1,8 +1,23 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::BaseController
   before_filter :authorize_admin!
 
   def index
-    #
+    @users = User.all(:order => "email")
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to admin_users_path
+    else
+      flash[:notice] = "User has not been created."
+      render :action => "new"
+    end
   end
 
 end
